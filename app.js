952d096e3d595065b5b0a1e0d3bfb39086d91391
Priya Kothalkar <priyakothalkar2973@gmail.com>
2021-07-17 15:34:03 +0530

    updated minion API

--- app.js
+++ app.js
@@ -3,24 +3,29 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector('#output');
 
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function getTranslationURL(text) {
-    return serverURL + "?" + "text=" + text;
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input;
 };
 
+//for errors we have a function
 function errorHandler(error) {
     console.log("error occured", error);
 }
 
+//what to dowhen we have a input or a click
 function clickHandler() {
     // outputDiv.innerText = "translated " + txtInput.value;
-
     var inputText = txtInput.value;
-
-    fetch(getTranslationURL(inputText)).then(response => response.json()).then(json => console.log(json.contents.translated)).catch(errorHandler)
-
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
 };
-
 btnTranslate.addEventListener("click", clickHandler);
